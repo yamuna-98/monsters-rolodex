55887d9b3eb0c41230ce57edeacc4c2058a145a3
yamuna-98 <imyamuna.sureddy@gmail.com>
2022-04-12 13:26:38 +0530

    addded a comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
+// this is a class component.
 class App extends Component {
   constructor() {
     super();
